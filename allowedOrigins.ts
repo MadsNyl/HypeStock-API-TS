3a04b31205cdc672a25d62f792fe41f1570365d1
Mads Nylund <madsnyl@gmail.com>
2023-06-25 10:34:01 +0200

    altered cors for allowing origins

---
+++ allowedOrigins.ts
@@ -0,0 +1,10 @@
+
+
+const allowedOrigins = [
+    'https://www.hypestock.net',
+    'http://127.0.0.1:5173',
+    'http://localhost:5173'
+];
+
+
+export default allowedOrigins;
--- middleware/credentials.ts
+++ middleware/credentials.ts
@@ -1,10 +1,18 @@
 import { NextFunction, Request, Response } from "express";
-
+import allowedOrigins from "../allowedOrigins";
 
 const credentials = (req: Request, res: Response, next: NextFunction) => {
     const origin = req.headers.origin;
+
+    if (origin && allowedOrigins.includes(origin)) {
+
+    }
+
     if (origin === "http://localhost:5173") {
         res.header('Access-Control-Allow-Credentials', "true");
+        res.header('Access-Control-Allow-Methods', ["GET", "POST", "DELETE", "PUT", "OPTIONS"]);
+        res.header('Access-Control-Allow-Headers', ["Origin", "Content-Type", "Accept", "Authorization"]);
+        res.header('Access-Control-Allow-Origin', origin);
     }
     next();
 }
